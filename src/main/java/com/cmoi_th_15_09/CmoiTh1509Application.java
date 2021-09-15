139e2ec3fea678f3bd0e39859ff7619804ddf61f
Phuoc <tdphuoc77@gmail.com>
2021-09-15 08:11:01 +0700

    first commit

---
+++ src/main/java/com/cmoi_th_15_09/CmoiTh1509Application.java
@@ -0,0 +1,13 @@
+package com.cmoi_th_15_09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmoiTh1509Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CmoiTh1509Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/cmoi_th_15_09/CmoiTh1509ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmoi_th_15_09;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmoiTh1509ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
